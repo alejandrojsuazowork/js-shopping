31bc52dd6a9bb298393f1d3be085b10157ead346
alejandrojsuazowork <alejandrojsuazowork@gmail.com>
2019-11-11 10:00:50 -0600

    Fixing coverage

--- ShoppingCartBackend/src/main/java/com/fake/real/server/ShoppingCartBackend/ShoppingCartBackendApplication.java
+++ ShoppingCartBackend/src/main/java/com/fake/real/server/ShoppingCartBackend/ShoppingCartBackendApplication.java
@@ -8,15 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class ShoppingCartBackendApplication {
 
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("*");
-			}
-		};
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingCartBackendApplication.class, args);
 	}
